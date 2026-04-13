835bc627f0d522a08522efb5757579d3d441319a
animuscrmsfdc <animuscrm@gmail.com>
2026-04-13 12:49:09 +0200

    Fixing elements in existing LWC of the org - removing tech debt due to legacy best practices

--- force-app/main/default/lwc/bearLocation/bearLocation.js
+++ force-app/main/default/lwc/bearLocation/bearLocation.js
@@ -1,14 +1,11 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-// Set Bear object fields
-const NAME_FIELD = 'Bear__c.Name';
-const LOCATION_LATITUDE_FIELD = 'Bear__c.Location__Latitude__s';
-const LOCATION_LONGITUDE_FIELD = 'Bear__c.Location__Longitude__s';
-const bearFields = [
-	NAME_FIELD,
-	LOCATION_LATITUDE_FIELD,
-	LOCATION_LONGITUDE_FIELD
-];
+import NAME_FIELD from '@salesforce/schema/Bear__c.Name';
+import LOCATION_LATITUDE_FIELD from '@salesforce/schema/Bear__c.Location__Latitude__s';
+import LOCATION_LONGITUDE_FIELD from '@salesforce/schema/Bear__c.Location__Longitude__s';
+
+const bearFields = [NAME_FIELD, LOCATION_LATITUDE_FIELD, LOCATION_LONGITUDE_FIELD];
+
 export default class BearLocation extends LightningElement {
   @api recordId;
   name;
@@ -18,11 +15,9 @@ export default class BearLocation extends LightningElement {
     if (error) {
       // TODO: handle error
     } else if (data) {
-      // Get Bear data
-      this.name =  getFieldValue(data, NAME_FIELD);
+      this.name = getFieldValue(data, NAME_FIELD);
       const Latitude = getFieldValue(data, LOCATION_LATITUDE_FIELD);
       const Longitude = getFieldValue(data, LOCATION_LONGITUDE_FIELD);
-      // Transform bear data into map markers
       this.mapMarkers = [{
         location: { Latitude, Longitude },
         title: this.name,
